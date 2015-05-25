1e8e2f2d51d22f661f95463c87ce6ae5a9ba4144
Andre Girard <a@andre-girard.com>
2015-05-24 22:06:13 -0400

    change todoapp to db in databe uri

--- todoapp.py
+++ todoapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://root:jjbVaAgNrBNV7kD0@172.17.42.1:49153/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://root:jjbVaAgNrBNV7kD0@172.17.42.1:49153/db'
 
 from views import *
 
